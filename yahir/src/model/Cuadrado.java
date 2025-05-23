b70be46236a6b9153c99aa529d84c78d7d3ff4e6
YhirLuna <yahirluna@correo.com>
2025-05-22 20:28:03 -0600

    Agregué App.java y carpeta model con clases Circulo y Triangulo y el cuadrado

---
+++ yahir/src/model/Cuadrado.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Cuadrado {
+
+    private double lado;
+
+    public Cuadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getArea() {
+        return lado * lado;
+    }
+
+    public double getPerimetro() {
+        return 4 * lado;
+    }
+
+}
